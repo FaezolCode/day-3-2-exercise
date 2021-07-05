027aa238fe82dbfe9b4a8e58d62bfd775236dc4a
AminuFaezol <>
2021-07-05 09:28:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+result = round (weight / height **2)
+
+if 
+
+
+
+
